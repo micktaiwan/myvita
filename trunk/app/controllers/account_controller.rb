a74376f9e5a8b640ed3eff924a3c04b2e8e589b3
faivrem <faivrem@0abb816f-df36-0410-937b-5763c1c61933>
2009-05-31 15:32:49 +0000

    updated to rails 2.3.2

--- trunk/app/controllers/account_controller.rb
+++ trunk/app/controllers/account_controller.rb
@@ -6,26 +6,26 @@ class AccountController < ApplicationController
    end
 
    def login
-    case @request.method
+    case request.method
       when :post
-        if @session['user'] = User.authenticate(@params['user_login'], @params['user_password'])
+        if session['user'] = User.authenticate(params['user_login'], @params['user_password'])
           #AppMailer.deliver_alert("Login: #{@session['user']['name']}","#{@session['user']['name']} just logged on PTM")
           flash['notice']  = "Login successful"
           redirect_back_or_default :controller => "myvita"
         else
-          @login    = @params['user_login']
+          @login    = params['user_login']
           @message  = "Login unsuccessful"
       end
     end
    end
 
    def signup
-    case @request.method
+    case request.method
       when :post
-        @user = User.new(@params['user'])
+        @user = User.new(params['user'])
         
         if @user.save      
-          @session['user'] = User.authenticate(@user.login, @params['user']['pwd'])
+          session['user'] = User.authenticate(@user.login, params['user']['pwd'])
           #AppMailer.deliver_alert("Signup: #{@session['user']['name']}","#{@session['user']['name']} just sign up on PTM")
           flash['notice']  = "Signup successful"
           redirect_back_or_default :controller => "myvita"          
@@ -36,15 +36,15 @@ class AccountController < ApplicationController
    end  
 
    def delete
-    if @params['id']
-      @user = User.find(@params['id'])
+    if params['id']
+      @user = User.find(params['id'])
       @user.destroy
     end
     redirect_back_or_default :controller => "myvita"
    end  
     
    def logout
-    @session['user'] = nil
+    session['user'] = nil
    end
     
    def welcome
--- trunk/app/controllers/myvita_controller.rb
+++ trunk/app/controllers/myvita_controller.rb
@@ -14,40 +14,40 @@ class MyvitaController < ApplicationController
   end
   
   def add_descr
-    step_id = @params['sid']
+    step_id = params['sid']
     d = Step.find(step_id).descrs.create
     d.save		
     render :partial => 'descr_edit', :locals=>{:descr_edit=>d}
   end
   
   def edit
-    @edit = Step.find(@params['id'])
+    @edit = Step.find(params['id'])
     render :partial => "edit", :locals=>{:edit=>@edit}
   end
   
   def edit_descr
-    d = Descr.find(@params['id'])
+    d = Descr.find(params['id'])
     render :partial => "descr_edit", :locals=>{:descr_edit=>d}
   end	
   
   def delete
-    Step.find(@params['id']).destroy
+    Step.find(params['id']).destroy
     render_text 'Deleted !'
   end	
   
   def delete_descr
-    d = Descr.find(@params['id'])
+    d = Descr.find(params['id'])
     d.destroy
     render_text 'Deleted !'
   end
   
   def save_changes
-    d = Step.update(@params['id'],@params['edit'])
+    d = Step.update(params['id'],params['edit'])
     render :partial => "step", :locals=>{:step=>d }
   end
   
   def save_descr
-    d = Descr.update(@params['id'],@params['d'])
+    d = Descr.update(params['id'],params['d'])
     render :partial => "step", :locals=>{:step=>d.step }
   end
   
